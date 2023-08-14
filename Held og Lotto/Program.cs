f6b6791698220d0b25409dcdaa9df0d2853f7590
Ksakill <132750722+Ksakill@users.noreply.github.com>
2023-08-14 15:19:49 +0200

    Add project files.

---
+++ Held og Lotto/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading;
+
+namespace Held_og_Lotto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int temp; 
+            int[] Mylotto = new int[8];
+
+            Random random = new Random(); 
+
+            for (int i = 0; i < 8; i++)
+            {
+                temp = random.Next(1, 36);
+                Mylotto[i] = temp;
+            }
+
+            Console.Write($"Lotto nummerne er: ");
+
+            for (int i = 0; i < 8; i++)
+            {
+                if (i == 7) 
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write(Mylotto[i] + " ");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.Write(Mylotto[i] + " ");
+                }
+
+                Thread.Sleep(2000); 
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+// once more got some help online to creeate this.
